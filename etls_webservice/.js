5aca8f41e46a0870fa8d41390a8b566a9c5a84cf
uccjuangomez <juan.gomezch@campusucc.edu.co>
2022-11-12 18:45:40 -0500

    evaluacion de conocimiento 2

---
+++ etls_webservice/.js
@@ -0,0 +1,1 @@
+{"data":[{"NumberToDollarsResult":"two dollars","dnum":2},{"NumberToDollarsResult":"three dollars","dnum":3},{"NumberToDollarsResult":"one dollar","dnum":1},{"NumberToDollarsResult":"thirteen dollars","dnum":13},{"NumberToDollarsResult":"five dollars","dnum":5},{"NumberToDollarsResult":"six dollars","dnum":6}]}
